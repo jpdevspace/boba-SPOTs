bb506ee437962c3c5e8825872b870091b72d0caa
Jean Paul G <jpgiraldo7@gmail.com>
2024-01-22 15:46:46 -0600

    [client] displaying each store's relevant info

--- client/src/components/ShopItem.jsx
+++ client/src/components/ShopItem.jsx
@@ -1,6 +1,21 @@
 const ShopItem = ({ data }) => {
   console.log("Data >>> ", data);
-  return <div className="shop-item">{data.name}</div>;
+  return (
+    <div className="shop-item">
+      <div>
+        <img className="shop-img" src={data.image_url ?? ""} alt="Boba Shop" />
+      </div>
+      <div>
+        <p>{data.is_closed ? "Sorry store is closed" : "Store is open!"}</p>
+        <p className="shop-name">{data.name}</p>
+        <p>⭐️ {data.rating}</p>
+        <p>The store is {Math.floor(data.distance)} meters from you</p>
+        <p>
+          <a href={`tel:${data.phone}`}>Call Store</a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ShopItem;
